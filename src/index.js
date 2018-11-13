fc679f07970bebdc09b5ec1df2eb73a58a5a3ec0
darko <darkoservantes@outlook.com>
2018-11-13 12:36:55 +0300

    Add Vote function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { setContext } from 'apollo-link-context'
-import AUTH_TOKEN from './constants'
+import { AUTH_TOKEN } from './constants'
 
 
 const authLink = setContext((_, { headers }) => {
